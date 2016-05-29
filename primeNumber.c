23d0c6d350888f377880745c82d2c45fbe63c9c9
hiteder <hitenderps>
2016-05-29 15:00:49 +0530

    modified

--- primeNumber.c
+++ primeNumber.c
@@ -1,35 +1,46 @@
 #include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+#include<stdbool.h>
 
-main() {
+bool check_Prime(int num){
+	int i=num;
+	if(num==1)
+		return 1;
+	if(num==2)
+		return 1;
 
-
-	int T,j,i;
-	int M,N;
-	
-	scanf("%d",&T);
-	while(T--) {
-		
-		scanf("%d %d",&M,&N);
-		
-		for(i=M;i<=N;i++) {
-		   if(i==1)
-			continue;
-		   if(i%2==0)
-		       continue;
-		   if(i==3){
-			printf("%d\n",i);
-			continue;
-		   }
-		    else {
-			for(j=2;j<=i/2;j+=2){
-			    if(i%j!=0)
-			        continue;
-			    else
-				break;
+	if(num%2==0)
+		return 0;
+	if(num==3)
+		return 1;
+	else{
+		for(i=3;i<=(int)sqrt(num)+1;i+=2){
+			if(num%i==0){
+				return 0;
+				
 			}
-		        if(j>=i/2)
-			    printf("%d\n",i);
-		   }
+			if(i>=(int)sqrt(num))
+				return 1;
+
 		}
-        }
+	}
+	return 0;
 }
+
+int main(){
+	int start,end;
+
+	printf("enter the number start:- ");
+	scanf("%d",&start);
+	printf("enter the number end:- ");
+	scanf("%d",&end);
+
+	int i;
+	for(i=start;i<=end;i++){
+		if(check_Prime(i))
+			printf("%d\n",i);
+	}
+return 0;
+}
+
